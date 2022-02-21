d217a7103d8c0039ba50d09446984b4ff6d61d64
Bishal <bishalkpatel@gmail.com>
2022-02-21 15:55:06 +0530

    Node timezone project

---
+++ index.js
@@ -0,0 +1,14 @@
+const moment = require('moment-timezone');
+
+moment.tz.setDefault('Asia/Kolkata');
+
+let targetTimezone;
+
+if (process.argv.length != 3) {
+  console.log("Usages: node <script-file> <timezone>");
+  process.exit(1);
+} else {
+  targetTimezone = process.argv[2];
+}
+
+console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
